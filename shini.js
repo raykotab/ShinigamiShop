385fab426c0b275adb8535358a7b9dd639155343
JosephCrespin <67466171+JosephCrespin@users.noreply.github.com>
2020-10-09 12:01:25 +0200

    javascript

--- shini.js
+++ shini.js
@@ -1,1 +1,11 @@
 'use strict'
+let resultado = -10;
+alert(`¿Quieres un descuento?`);
+do {
+  var suma = prompt(`¿Cuanto es 10-20?`);
+  if (suma != resultado){
+    alert(`Te has quedado sin descuento :( `);
+  }
+} while (suma != resultado);
+
+confirm(`Obtienes un 10% de descuento`);
